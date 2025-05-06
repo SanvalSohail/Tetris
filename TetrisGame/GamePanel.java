d6bfe7d82d21d981cb175f9101cb5e6b54a5e5cc
Sanval Sohail <sanval.sohail@gmail.com>
2025-05-06 17:49:21 -0400

    Add files via upload
    Game skeleton
    -- void main string args
    -- draw loop (fps limited)
    -- box for play area
    + Gameplay.java
    + Gamepanel.java
    + Tetris.java

---
+++ TetrisGame/GamePanel.java
@@ -0,0 +1,58 @@
+package TetrisGame;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
+    Gameplay gm;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+
+        gm = new Gameplay();
+    }
+
+    public void launchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run(){
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+    public void update(){
+        gm.update();
+    }
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        gm.draw(g2);
+    }
+
+}
